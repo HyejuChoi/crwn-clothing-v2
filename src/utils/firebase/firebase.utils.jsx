5a08b5b5bbe46f9a61d32d5e8cb14fd2d305673f
HyejuChoi <choih2708@student.laccd.edu>
2023-11-28 10:18:22 -0800

    created sign up component, the form input component, button compnent

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -35,7 +35,12 @@ export const signInWithGoogleRedirect = () =>
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth, additionalInformation) => {
+export const createUserDocumentFromAuth = async (
+    userAuth, 
+    additionalInformation = {}
+    ) => {
+    if (!userAuth) return;
+    
     const userDocRef = doc(db, 'users', userAuth.uid);
 
     const userSnapshot = await getDoc(userDocRef);
@@ -48,7 +53,8 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInformation
             await setDoc(userDocRef, {
                 displayName,
                 email,
-                createdAt
+                createdAt,
+                ...additionalInformation,
             });
         } catch (error) {
             console.log('error creating the user', error.message);
@@ -56,4 +62,10 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInformation
     }
 
     return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email || !password) return;
+
+    return await createUserWithEmailAndPassword(auth, email, password);    
 };
